35ed21bd9849dce07b1ca11a4ef1e7d905ed5053
jayankoliya <jayankoliya03@gmail.com>
2023-07-06 09:42:55 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,20 @@ var router = express.Router();
 const { Register, Log_in, Update, delete_user, page_no  } = require('../controller/UserController')
 
 // data insert 
-router.post('/',Log_in);    
-router.post('/register',Register);
-router.post('/update/:id',Update);                             
-router.get('/delete/:id',delete_user);                             
-router.get('/:page',page_no);                             
 
 router.get('/',function(req,res,next){
     res.render('index',{title: 'Express Index'})
 })
 
 router.get('/dashboard',function(req,res,next){
-    res.render('dashboard',{title: 'Dashboard index'})
+    res.render('dashboard',{title: 'Express dashboard'})
 })
+
+router.post('/',Log_in);    
+router.post('/register',Register);
+router.post('/update/:id',Update);                             
+router.get('/delete/:id',delete_user);                             
+router.get('/:page',page_no);                             
 
 
 module.exports = router;
